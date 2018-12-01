14b0517fff1335652cca8875177dd55a684c5e01
Humberto Arriaga <humberto.arriaga@dieboldnixdorf.com>
2018-12-01 01:07:31 -0600

    Add initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+import { RouterModule, Routes, Router } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'edit/:id', component: EditComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +24,8 @@ import { EditComponent } from './components/edit/edit.component';
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
